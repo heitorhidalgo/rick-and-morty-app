07675eaac21e49626b64582a5d2ac3cbf13e69f4
heitorhidalgo <heitorhidalgo10@hotmail.com>
2025-12-30 15:50:18 -0300

    feat: implementacao da list, cardPersonagem

--- lib/personagem_component.dart
+++ lib/personagem_component.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:rick_morty_app/personagem_controller.dart';
 import 'package:rick_morty_app/theme_controller.dart';
 
 mixin class PersonagemComponent {
-  final PersonagemController controller = PersonagemController();
+  final PersonagemController controller = Get.put(PersonagemController());
 
   late Orientation orientacao;
 
@@ -14,9 +15,30 @@ mixin class PersonagemComponent {
 
   Widget body() {
     return SafeArea(
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(children: [mudarTema(), logo()]),
+      child: Column(
+        children: [
+          mudarTema(),
+          logo(),
+          const SizedBox(height: 10),
+
+          const Text(
+            'Personagens',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: Obx(
+              () => ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemCount: controller.personagens.length,
+                itemBuilder: (context, index) {
+                  final item = controller.personagens[index];
+                  return cardPersonagem(item);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -26,8 +48,8 @@ mixin class PersonagemComponent {
       heightFactor: 1,
       child: Image.asset(
         'assets/images/logo.png',
-        width: orientacao == Orientation.portrait ? 300 : 200,
-        height: orientacao == Orientation.portrait ? 300 : 200,
+        width: orientacao == Orientation.portrait ? 300 : 180,
+        height: orientacao == Orientation.portrait ? 300 : 180,
       ),
     );
   }
@@ -41,6 +63,74 @@ mixin class PersonagemComponent {
           onPressed: () => ThemeController().switchTheme(),
         ),
       ],
+    );
+  }
+
+  Widget cardPersonagem(Map<String, String> item) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(15),
+              bottomLeft: Radius.circular(15),
+            ),
+            child: Image.network(
+              item['image']!,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, progress) {
+                if (progress == null) return child;
+                return const SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              },
+
+              errorBuilder:
+                  (context, error, stackTrace) => const SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Icon(Icons.error),
+                  ),
+            ),
+          ),
+          const SizedBox(width: 15),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  item['name']!,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.circle,
+                      size: 12,
+                      color:
+                          item['status'] == 'alive' ? Colors.green : Colors.red,
+                    ),
+                    const SizedBox(width: 5),
+                    Text('${item['status']} - ${item['species']}'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/personagem_controller.dart
+++ lib/personagem_controller.dart
@@ -3,4 +3,26 @@ import 'package:get/get.dart';
 
 class PersonagemController extends GetxController {
   late BuildContext context;
+
+  final RxList<Map<String, String>> personagens =
+      <Map<String, String>>[
+        {
+          'name': 'Rick Sanchez',
+          'status': 'alive',
+          'image': 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+          'species': 'human',
+        },
+        {
+          'name': 'Morty Smith',
+          'status': 'alive',
+          'image': 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+          'species': 'human',
+        },
+        {
+          'name': 'Summer Smith',
+          'status': 'alive',
+          'image': 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+          'species': 'human',
+        },
+      ].obs;
 }
